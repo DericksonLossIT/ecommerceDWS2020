7ed5a3fdcec6674da1847aece3bcbd9f48bb1f19
DericksonLossIT <dericksonlossit@gmail.com>
2020-06-01 20:15:58 -0300

    adicionando camadas no index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8cd8e75688.js" crossorigin="anonymous"></script>
+  
+
+</head>
+<body>
+    <div class="header">Header</div>
+    <div class="">
+        <i class="fas fa-bars"></i>
+    </div>
+    <div class="content">Conteudo
+        <div class="card">Card</div>
+    
+    </div>
+    <div class="footer">Footer</div>
+    
+</body>
+</html>
