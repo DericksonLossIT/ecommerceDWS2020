f11c44dfb196c3100148b230ee54491c4b807c62
DericksonLossIT <dericksonlossit@gmail.com>
2020-06-01 18:53:42 -0300

    Adicionando index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skies Supply</title>
+</head>
+<body>
+    <button>Clique aqui para descobrir</button>
+</body>
+</html>
