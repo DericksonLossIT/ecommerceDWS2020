37b85eb1157e18303f0fd0f1a00098681293386b
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2020-06-02 17:40:11 -0300

    construindo o conteudo da pagina

--- index.php
+++ index.php
@@ -7,19 +7,26 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/8cd8e75688.js" crossorigin="anonymous"></script>
-  
-
 </head>
+
 <body>
     <div class="header">Header</div>
     <div class="">
         <i class="fas fa-bars"></i>
     </div>
     <div class="content">Conteudo
-        <div class="card">Card</div>
-    
-    </div>
+        <div class="card">
+            <h5 class="card-title">Card title</h5>
+            <img src="img/cam1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$40,00</p>
+            </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+        </div>
+    </div>    
     <div class="footer">Footer</div>
-    
 </body>
 </html>
