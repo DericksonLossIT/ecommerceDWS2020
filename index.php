0772a916cc41091cf4114fc64f8dd9997f227e53
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2020-06-07 20:43:11 -0300

    Modificando a cor do Footer, Header e Content - Modidificando o Content, adicionando novos cards

--- index.php
+++ index.php
@@ -4,29 +4,157 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/8cd8e75688.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div class="header">Header</div>
+    <div class="header"></div>
     <div class="">
         <i class="fas fa-bars"></i>
     </div>
-    <div class="content">Conteudo
+    <div class="content">
         <div class="card">
-            <h5 class="card-title">Card title</h5>
-            <img src="img/cam1.jpg" class="card-img-top" alt="...">
+            <h5 class="card-title">Red Cap Nike</h5>
+            <img src="img/bone1.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                    <p class="card-text">R$40,00</p>
+                    <p class="card-text">R$100,00</p>
             </div>
                 <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
                 </div>
         </div>
-    </div>    
-    <div class="footer">Footer</div>
+        <div class="card">
+            <h5 class="card-title">Black Cap NY</h5>
+            <img src="img/bone2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$50,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Yellow Robot T-Shirt</h5>
+            <img src="img/cam1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$35,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">White Bender T-Shirt</h5>
+            <img src="img/cam2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$35,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Black T-Shirt</h5>
+            <img src="img/cam3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$35,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Light BLue Sweater</h5>
+            <img src="img/sweat1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$80,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Black Sweater</h5>
+            <img src="img/sweat2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$100,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">BLack BLS Sweater</h5>
+            <img src="img/sweat3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$115,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Black Jacket</h5>
+            <img src="img/jack1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$400,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Brown Jacket</h5>
+            <img src="img/jack2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$350,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        <div class="card">
+            <h5 class="card-title">Blue Jacket</h5>
+            <img src="img/jack3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$300,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Gray Shoes</h5>
+            <img src="img/ten1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$200,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Black Tennis Skater</h5>
+            <img src="img/ten2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$200,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+        <div class="card">
+            <h5 class="card-title">Brown All-Star</h5>
+            <img src="img/ten3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                    <p class="card-text">R$150,00</p>
+            </div>
+                <div class="card-body">
+                    <button type="submit" id="add"><i class="fas fa-cart-plus"></i></button>
+                </div>
+        </div>
+    </div>
+    <div class="footer"></div>
 </body>
 </html>
